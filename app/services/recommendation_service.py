1da02b6efa223cef41e02886bc1bf2db22e341b4
pranaviredd <160719184+pranaviredd@users.noreply.github.com>
2026-05-27 20:16:46 +0530

    Added recommendation service

---
+++ app/services/recommendation_service.py
@@ -0,0 +1,9 @@
+class RecommendationService:
+
+    def recommend(self, stage):
+
+        return {
+            "stage": stage,
+            "medicine": "Donepezil",
+            "reminder": "Take once daily"
+        }
